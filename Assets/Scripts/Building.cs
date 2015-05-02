064a495f6e45237bcc0fc6c8578d9838ab40e90f
Jason M <johnred32@gmail.com>
2015-05-02 03:41:21 -0400

    Created Building Classes
    Also created Resources enum

---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Superclass for all buildings
+ */
+public abstract class Building : MonoBehaviour {
+
+	/** Who owns this building */
+	public int lot; //TODO: change int to lot
+
+	public void Demolish() {
+		Destroy (this.gameObject);
+	}
+}
---
+++ Assets/Scripts/PotteryWorkshop.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PotteryWorkshop : Workshop {
+
+	public PotteryWorkshop () {
+		resourceUsed = Resources.Clay;
+		goodProduced = Resources.Pottery;
+	}
+
+	void Start () {
+
+	}
+	
+
+	void Update () {
+
+	}
+}
