f61bf39c853a72847e9135dcc5d1a22da8f6d1f2
Michael Knooihuisen <mknooihuisen@gmail.com>
2015-05-02 12:39:13 -0400

    added selection calculation code

--- Assets/Scripts/GameDirector.cs
+++ Assets/Scripts/GameDirector.cs
@@ -4,12 +4,15 @@ using System.Collections;
 public class GameDirector : MonoBehaviour {
 
 	public int lotsPerSite;
+
+	public GameObject selectedObject;
 	
 	public Site site;
 
 	// Use this for initialization
 	void Start () {
-		Instantiate (site.SitePlane);
+		GameObject go = (GameObject)Instantiate (site.SitePlane);
+		site.SitePlane = go;
 		for (int i = 0; i < lotsPerSite; i++) {
 			site.NewLot();
 		}
@@ -18,9 +21,20 @@ public class GameDirector : MonoBehaviour {
 	public void MakeBuilding(Building b) {
 		site.Lots[0].NewBuilding (b);
 	}
-	
+
+	private GameObject calculateSelectedObject() {
+		Debug.Log ("Click");
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+		Physics.Raycast (ray, out hit);
+		Debug.Log ("Hit at: " + hit.point);
+		return hit.collider.gameObject;
+	}
 	// Update is called once per frame
 	void Update () {
+
+		if (Input.GetMouseButtonDown (0))
+			selectedObject = calculateSelectedObject ();
 	
 	}
 }
