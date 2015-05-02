83a75150a7b3d93f207073b8286722e1b3dc683d
Tanner Swett <tannerswett@gmail.com>
2015-05-02 03:39:53 -0400

    Create Lot.cs, Site.cs

---
+++ Assets/Scripts/Lot.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lot : MonoBehaviour {
+	public Site Site { get; private set; }
+	public Building Building { get; private set; }
+	public Business Owner { get; set; }
+	public Resource? Resource { get; private set; }
+
+	public Lot (Site site) {
+		Site = site;
+	}
+
+	public void DestroyBuilding () {
+		if (Building != null) {
+			Building.Demolish ();
+			Building = null;
+		}
+	}
+
+	public BuildingT NewBuilding<BuildingT> () where BuildingT : Building, new() {
+		DestroyBuilding ();
+
+		BuildingT newBuilding = new BuildingT ();
+		Building = newBuilding;
+		return newBuilding;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Site.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Site : MonoBehaviour {
+	public List<Lot> Lots { get; private set; }
+
+	public Lot NewLot() {
+		Lot newLot = new Lot (this);
+		Lots.Add (newLot);
+		return newLot;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
