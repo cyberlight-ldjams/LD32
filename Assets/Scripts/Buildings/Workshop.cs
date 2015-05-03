d8b2141f0256abfaf9a3afee604ffb26bd485d45
Tanner Swett <tannerswett@gmail.com>
2015-05-03 15:34:19 -0400

    Make workshops produce stuff

--- Assets/Scripts/Buildings/Workshop.cs
+++ Assets/Scripts/Buildings/Workshop.cs
@@ -14,4 +14,17 @@ public abstract class Workshop : Building {
 
 	/** The goods out per goods put in */
 	public abstract float inOutRatio { get; }
+
+	public override void Produce () {
+		double inAmount = 0.1 / inOutRatio;
+
+		double oldAmountInput = owner.myInventory.getAmountOfAt(resourceUsed, site);
+
+		if (oldAmountInput >= inAmount) {
+			owner.myInventory.setAmountOfAt(resourceUsed, site, oldAmountInput - inAmount);
+
+			double oldAmountOutput = owner.myInventory.getAmountOfAt(goodProduced, site);
+			owner.myInventory.setAmountOfAt(goodProduced, site, oldAmountOutput + 0.1);
+		}
+	}
 }
