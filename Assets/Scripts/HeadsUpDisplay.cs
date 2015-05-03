248468398d96ddc044708e9bec0b00f80823c758
Michael Knooihuisen <mknooihuisen@gmail.com>
2015-05-03 03:46:59 -0400

    Did lots of stuff

--- Assets/Scripts/HeadsUpDisplay.cs
+++ Assets/Scripts/HeadsUpDisplay.cs
@@ -8,10 +8,27 @@ public class HeadsUpDisplay : MonoBehaviour {
 	public GameObject potteryText;
 	public PlayerBusiness business;
 	public Site currentSite;
+	public GameDirector gameDirector;
+
+	private Button quarryBtn;
+	private Button workshopBtn;
+
 
 	// Use this for initialization
 	void Start () {
-	
+		Button[] buttons = Object.FindObjectsOfType<Button> ();
+		foreach (Button but in buttons) {
+			string butName = but.name;
+
+			switch (butName) {
+			case  "QuarryButton":
+				quarryBtn = but;
+				break;
+			case "WorkshopButton":
+				workshopBtn = but;
+				break;
+			}
+		}
 	}
 	
 	// Update is called once per frame
@@ -20,6 +37,18 @@ public class HeadsUpDisplay : MonoBehaviour {
 		clayText.GetComponent<Text>().text = string.Format("Clay: {0}", clayAmount);
 
 		double potteryAmount = business.myInventory.getAmountOfAt(Resource.Pottery, currentSite);
-		potteryText.GetComponent<Text>().text = string.Format("Pottery: {0}", potteryAmount);
+		potteryText.GetComponent<Text> ().text = string.Format ("Pottery: {0}", potteryAmount);
+
+
+		//dissalow placement before selection
+		if (gameDirector.selectedObject == null) {
+			quarryBtn.enabled = false;
+			workshopBtn.enabled = false;
+		} else {
+			quarryBtn.enabled = true;
+			quarryBtn.enabled = true;
+		}
+
+
 	}
 }
