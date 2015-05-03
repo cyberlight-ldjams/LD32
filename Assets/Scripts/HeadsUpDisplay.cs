d872274c436923bbf42c28381e34b2357a1a7a8b
Michael Knooihuisen <mknooihuisen@gmail.com>
2015-05-03 17:16:10 -0400

    Moved RandomEventUI code out of HeadsUpDisplay class

--- Assets/Scripts/HeadsUpDisplay.cs
+++ Assets/Scripts/HeadsUpDisplay.cs
@@ -10,16 +10,10 @@ public class HeadsUpDisplay : MonoBehaviour {
 	public Site currentSite;
 	public GameDirector gameDirector;
 
-	public GameObject dialog;
-
-	private Button eventBtn1, eventBtn2, eventBtn3, eventBtn4;
-
-	private Slider eventTimerSlider;
-
 
 	private Button quarryBtn;
 	private Button workshopBtn;
-	private Text title, description, afterText;
+
 
 
 	// Use this for initialization
@@ -36,44 +30,6 @@ public class HeadsUpDisplay : MonoBehaviour {
 				workshopBtn = but;
 				break;
 			
-			case "REButton1":
-				eventBtn1 = but;
-				break;
-			case "REButton2":
-				eventBtn2 = but;
-				break;
-			case "REButton3":
-				eventBtn3 = but;
-				break;
-			case "REButton4":
-				eventBtn4 = but;
-				break;
-			}
-
-		}
-
-		Text [] temp = dialog.GetComponentsInChildren<Text> ();
-		foreach (Text t in temp) {
-			string textName = t.name;
-
-			switch (textName) {
-			case "Title":
-				title = t;
-				break;
-			case "Description":
-				description = t;
-				break;
-			case "afterText":
-				afterText = t;
-				break;
-			}
-		}
-
-		Slider [] s = Object.FindObjectsOfType<Slider> ();
-		foreach (Slider s1 in s) {
-			if (s1.name.Equals ("Timer")) {
-				eventTimerSlider = s1;
-				break;
 			}
 		}
 
--- Assets/Scripts/RandomEventManager.cs
+++ Assets/Scripts/RandomEventManager.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using System;
+using UnityEngine.UI;
 
 public class RandomEventManager : MonoBehaviour {
 
@@ -19,6 +19,13 @@ public class RandomEventManager : MonoBehaviour {
 	private RandomEvent current;
 	private float currentTime, endTime;
 
+	public GameObject dialog;
+	
+	private Button eventBtn1, eventBtn2, eventBtn3, eventBtn4;
+	
+	private Slider eventTimerSlider;
+	private Text title, description, afterText;
+
 
 	public RandomEventManager(List<Business> businesses, List<Site> sites) {
 		this.businesses = businesses;
@@ -29,10 +36,50 @@ public class RandomEventManager : MonoBehaviour {
 	void Start () {
 		choices = RandomEventPool.get (businesses, sites);
 		used = new List<RandomEvent>();
+		Button [] b;
+		foreach(Button but in Object.FindObjectsOfType<Button>()) {
+			switch(but.name) {
+				case "REButton1":
+				eventBtn1 = but;
+				break;
+				case "REButton2":
+				eventBtn2 = but;
+				break;
+				case "REButton3":
+				eventBtn3 = but;
+				break;
+				case "REButton4":
+				eventBtn4 = but;
+				break;
+			}
+		}	
+
+	Text [] temp = dialog.GetComponentsInChildren<Text> ();
+	foreach (Text t in temp) {
+		string textName = t.name;
+		
+		switch (textName) {
+		case "Title":
+			title = t;
+			break;
+		case "Description":
+			description = t;
+			break;
+		case "afterText":
+			afterText = t;
+			break;
+		}
+	}
+
+	Slider [] s = Object.FindObjectsOfType<Slider> ();
+	foreach (Slider s1 in s) {
+		if (s1.name.Equals ("Timer")) {
+			eventTimerSlider = s1;
+			break;
+		}
+	}
 
 		pick ();
-
-
 	}
 	
 	// Update is called once per frame
