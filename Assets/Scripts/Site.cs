ba5a207c987ade341dde6ca25ac49e63d453812f
Jason M <johnred32@gmail.com>
2015-05-03 04:22:15 -0400

    Allowed world to create sites

--- Assets/Scripts/Site.cs
+++ Assets/Scripts/Site.cs
@@ -10,26 +10,51 @@ public class Site {
 	private Vector2 current;
 
 	public Site (int lotsPerSite, Business owner) {
-		current = new Vector2 (-rows/2.0f + 0.5f, -cols/2.0f + 0.5f);
 		Lots = new List<Lot> ();
+		current = new Vector2(0, 0);
 
-		GameObject go = (GameObject) Object.Instantiate (Resources.Load ("SitePlane"));
+		GameObject go;
+		go = (GameObject) Object.Instantiate (Resources.Load ("SitePlane"));
 		SitePlane = go;
+		current = new Vector2 (-rows/2.0f + 0.5f + go.transform.position.x, 
+		                       -cols/2.0f + 0.5f + go.transform.position.z);
 		for (int i = 0; i < lotsPerSite; i++) {
 			NewLot(owner);
 		}
+		MoveLots();
 	}
 
 	private Lot NewLot(Business owner) {
-		if (current.x > (rows/2.0f)) {
-			current.x = -rows/2.0f + 0.5f;
-			current.y++;
-		}
-
 		Lot newLot = new Lot (this, owner, new Vector3 (current.x * 10.1f, 0.1f, current.y * 10.1f), SitePlane.transform.rotation);
 		Lots.Add (newLot);
 
-		current.x++;
 		return newLot;
+	}
+
+	private void MoveLots() {
+		current = new Vector2 (-rows/2.0f + 0.5f, 
+		                       -cols/2.0f + 0.5f);
+		foreach (Lot l in Lots) {
+			if (current.x > (rows/2.0f)) {
+				current.x = -rows/2.0f + 0.5f;
+				current.y++;
+			}
+
+				l.getLotPlane().transform.position = 
+					new Vector3 (current.x * 10.1f + SitePlane.transform.position.x, SitePlane.transform.position.y + 0.05f, 
+					             current.y * 10.1f + SitePlane.transform.position.z);
+
+				current.x++;
+			
+		}
+	}
+
+	public void placeSite(Vector3 location) {
+		SitePlane.transform.position = location;
+		MoveLots();
+	}
+
+	public Vector3 getPlaneLocation() {
+		return SitePlane.transform.position;
 	}
 }
