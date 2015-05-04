10c1428264cb8bce338c33ee4613e2ebd91ba61c
Tanner Swett <tannerswett@gmail.com>
2015-05-04 16:46:17 -0400

    Make random resources show up

--- Assets/Scripts/Resource.cs
+++ Assets/Scripts/Resource.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEngine;
 
 public enum Resource
 {
@@ -10,7 +11,30 @@ public enum Resource
 public static class ResourceExtensions {
 	public static Resource[] NaturalResources = new Resource[] { Resource.Clay, Resource.Stone, Resource.Iron, Resource.Fish, Resource.Meat, Resource.Timber, Resource.Oil, Resource.Gold };
 
-	private static Random randGen = new Random();
+	public static Material Material(this Resource resource) {
+		switch (resource) {
+		case Resource.Clay:
+			return (Material)Resources.Load("ClayResource");
+		case Resource.Stone:
+			return (Material)Resources.Load("StoneResource");
+		case Resource.Iron:
+			return (Material)Resources.Load("IronResource");
+		case Resource.Fish:
+			return (Material)Resources.Load("FishResource");
+		case Resource.Meat:
+			return (Material)Resources.Load("MeatResource");
+		case Resource.Timber:
+			return (Material)Resources.Load("TimberResource");
+		case Resource.Oil:
+			return (Material)Resources.Load("OilResource");
+		case Resource.Gold:
+			return (Material)Resources.Load("GoldResource");
+		default:
+			return new Material("");
+		}
+	}
+
+	private static System.Random randGen = new System.Random();
 
 	public static List<T> ChooseRandom<T>(T[] list, int count) {
 		List<T> result = new List<T>();
