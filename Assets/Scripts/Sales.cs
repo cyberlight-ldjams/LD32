cd010dd8a603b829148852caca841c84e2191d1a
Michael Knooihuisen <mknooihuisen@gmail.com>
2015-05-04 13:14:05 -0400

    Added basic sales class

---
+++ Assets/Scripts/Sales.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sales : MonoBehaviour {
+
+	public int [] totalSales { get; private set; }
+
+	private GameTime<bool> quarters;
+
+	public void sell (Business seller, Resource r, int ammount, Business customer = null) {
+
+
+	}
+
+}
